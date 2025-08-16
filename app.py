ae8b8b97f388c6c427de6409a82499c0ce272ebc
Thanoo <homefoodautomation@gmail.com>
2025-08-16 13:11:19 +0700

    Update app.py and requirements.txt

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def index():
     return "Server is running!"
 
- 
+# ------------------- Upload -------------------
 @app.route("/upload_image", methods=["POST"])
 def upload_image():
     data = request.json
@@ -39,10 +39,25 @@ def upload_image():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-# ดูภาพย้อนหลัง
+# ------------------- Get Image -------------------
 @app.route("/upload_image/<filename>")
 def get_uploaded_image(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+# ------------------- List Images -------------------
+@app.route("/list_images")
+def list_images():
+    try:
+        files = os.listdir(UPLOAD_FOLDER)
+        files.sort(reverse=True)  # เรียงจากใหม่ไปเก่า
+        urls = [
+            request.host_url + "upload_image/" + f
+            for f in files
+        ]
+        return jsonify({"images": urls})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
